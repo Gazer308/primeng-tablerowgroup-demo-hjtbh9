f4664d34f831e1b9491d6d7b4dcc373800723495
Gazer308 <71849693+Gazer308@users.noreply.github.com>
2022-09-29 13:29:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# primeng-tablerowgroup-demo-hjtbh9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/primeng-tablerowgroup-demo-hjtbh9)
---
+++ src/app/customerservice.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Customer } from './customer';
+
+@Injectable()
+export class CustomerService {
+  constructor(private http: HttpClient) {}
+
+  getCustomersMedium() {
+    return this.http
+      .get<any>('assets/example-data.json')
+      .toPromise()
+      .then((res) => <any[]>res.data)
+      .then((data) => {
+        return data;
+      });
+  }
+}
